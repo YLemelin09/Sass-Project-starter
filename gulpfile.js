81b7e2d1434db904bfcdbf52de0a1bd508c88004
Yannick Lemelin <yannick.lemelin@gmail.com>
2017-08-23 15:28:14 -0400

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass');
+
+//compile sass
+gulp.task('sass', function(){
+  return gulp.src(['src/scss/*.scss'])
+    .pipe(sass())
+    .pipe(gulp.dest('src/css'))
+    .pipe(browserSync.stream());
+});
+
+// watch and serv
+gulp.task('serve', ['sass'], function(){
+  browserSync.init({
+    server: './src'
+  });
+  gulp.watch(['src/scss/*.scss'],['sass']);
+  gulp.watch(['src/*.html']).on('change',browserSync.reload);
+});
+
+//default
+gulp.task('default',['serve']);
